6f4fa8a2e5518dd9cc09bb460e424e6460a202cb
brijeshagal <brijeshagal@gmail.com>
2025-01-28 20:12:46 +0530

    push for build

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ mongoose
 const UserData = mongoose.model("UserDetails", userSchema);
 
 // Load the Swagger JSON file
-const swaggerDocument = JSON.parse(fs.readFileSync("./swagger.json", "utf8"));
+const swaggerDocument = JSON.parse(fs.readFileSync("swagger.json", "utf8"));
 
 // Serve Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
