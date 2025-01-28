29a6a8cd18cb768490aca11d30c390d15cfeae06
brijeshagal <brijeshagal@gmail.com>
2025-01-28 18:41:32 +0530

    add tsx build

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,12 +6,58 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const helmet_1 = __importDefault(require("helmet"));
-const quote_1 = __importDefault(require("./router/quote"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const viem_1 = require("viem");
+const user_1 = __importDefault(require("./schema/user"));
+const uploadToDB_1 = require("./uploadToDB");
 const app = (0, express_1.default)();
 dotenv_1.default.config();
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
-app.use("/swap", quote_1.default); // Register user routes
-app.use("/bridge", quote_1.default); // Register user routes
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose_1.default
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+    console.log("✅ MongoDB Connected");
+    (0, uploadToDB_1.addMultipleUsers)();
+})
+    .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+const UserData = mongoose_1.default.model("UserDetails", user_1.default);
+app.get("/getUserAddresses", async (req, res) => {
+    try {
+        const users = await UserData.find({}, "address");
+        const userAddresses = users.map((user) => user.address);
+        res.json(userAddresses);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+app.get("/userData/:address", async (req, res) => {
+    try {
+        const { address } = req.params;
+        const userAddress = (0, viem_1.getAddress)(address);
+        const userData = await UserData.findOne({ address: userAddress });
+        if (!userData) {
+            res.status(404).json({ error: "User not found" });
+            return;
+        }
+        res.json(userData);
+    }
+    catch (error) {
+        console.error("Error fetching user data:", error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+app.get("/userData", async (_, res) => {
+    try {
+        const allUsers = await UserData.find({});
+        res.json(allUsers);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+const PORT = process.env.APP_PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Server Url: http://localhost:${PORT}`);
+});
